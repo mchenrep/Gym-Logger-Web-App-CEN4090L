6b4653504803f113dc5c16e3fe97510a38590cb4
myo44to <myotiom@gmail.com>
2026-09-22 12:17:06 -0400

    Integrate frontend navigation with Flask and updated  navigation bar styling (#1)

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/workout")
+def workout():
+    return render_template("workout.html")
+
+@app.route("/exercises")
+def exercises():
+    return render_template("exercises.html")
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
 # --------------------------- Main ------------------------------------
 if __name__ == "__main__":
     app.run()
